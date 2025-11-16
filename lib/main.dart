d4fdb4303e5c89df5df3cef41b6656ee70cbf2f4
Vadim <zilitskyi_ak22@nuwm.edu.ua>
2025-11-16 16:29:35 +0200

    lab 3.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,99 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyFirstApp());
-
-class MyFirstApp extends StatefulWidget {
-  @override
-  _MyFirstAppState createState() => _MyFirstAppState();
+void main() {
+  runApp(MyApp());
 }
 
-class _MyFirstAppState extends State<MyFirstApp> {
-  bool _loading = false;       // чи йде завантаження
-  double _progressValue = 0.0; // відсоток завантаження
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "StatelessWidget simple",
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
       home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 166, 26, 209),
         appBar: AppBar(
-          title: Text('My First App'),
-          centerTitle: true,
+          title: Text("Laborator 3"),
         ),
         body: Center(
-          child: _loading
-              ? Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    LinearProgressIndicator(value: _progressValue),
-                    SizedBox(height: 20),
-                    Text(
-                      '${(_progressValue * 100).round()}%',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontSize: 24,
-                      ),
-                    ),
-                  ],
-                )
-              : Text(
-                  'Text button to download',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,
-                  ),
-                ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _loading ? null : _startLoading,
-          child: Icon(Icons.cloud_download),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              HintLabel('Click "-" for decrement'),
+              SizedBox(height: 8.0),
+              CounterWidget(),
+              SizedBox(height: 8.0),
+              HintLabel('Click "+" for increment'),
+            ],
+          ),
         ),
       ),
     );
   }
+}
 
-  void _startLoading() {
-    setState(() {
-      _loading = true;
-      _progressValue = 0.0;
-    });
+// Власний віджет для підказок
+class HintLabel extends StatelessWidget {
+  final String text;
 
-    const oneSec = Duration(seconds: 1);
-    Timer.periodic(oneSec, (Timer t) {
-      setState(() {
-        _progressValue += 0.2; // збільшуємо прогрес на 20% кожну секунду
-        if (_progressValue >= 1.0) {
-          _loading = false;  // завершили завантаження
-          _progressValue = 0.0;
-          t.cancel();
-        }
-      });
-    });
+  const HintLabel(this.text, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(color: Colors.amber[200]),
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Text(
+          text,
+          style: TextStyle(color: Colors.grey[700]),
+        ),
+      ),
+    );
   }
+}
+
+// Кастомний StatefulWidget для лічильника
+class CounterWidget extends StatefulWidget {
+  @override
+  _CounterWidgetState createState() => _CounterWidgetState();
+}
+
+class _CounterWidgetState extends State<CounterWidget> {
+  int _count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: const Color.fromARGB(255, 67, 11, 221),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          IconButton(
+            onPressed: _decrement,
+            icon: Icon(Icons.remove, color: Colors.white),
+          ),
+          Text(
+            '$_count',
+            style: TextStyle(fontSize: 40, color: Colors.white),
+          ),
+          IconButton(
+            onPressed: _increment,
+            icon: Icon(Icons.add, color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _increment() => setState(() => _count++);
+  void _decrement() => setState(() => _count--);
 }
